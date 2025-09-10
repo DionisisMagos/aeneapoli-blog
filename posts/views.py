9c7147d16ef1ddcd2fe8ff315052a6b45e3ca1de
Dionisis Magos <fridimagician@gmail.com>
2025-09-10 22:51:56 +0300

    Align templates/views with model fields (excerpt/content/created)

--- posts/views.py
+++ posts/views.py
@@ -4,21 +4,25 @@ from .models import Post
 
 class PostListView(ListView):
     model = Post
+    template_name = "posts/post_list.html"
     paginate_by = 6
-    template_name = 'posts/post_list.html'
-    queryset = Post.objects.filter(published=True).order_by('-created')
+
+    def get_queryset(self):
+        return Post.objects.filter(published=True).order_by("-created")
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'posts/post_detail.html'
+    template_name = "posts/post_detail.html"
+    slug_field = "slug"
+    slug_url_kwarg = "slug"
 
 def about_view(request):
-    return render(request, 'pages/about.html')
+    return render(request, "pages/about.html")
 
 def contact_view(request):
-    return render(request, 'pages/contact.html')
+    return render(request, "pages/contact.html")
 
 def social_view(request):
     from django.conf import settings
-    links = getattr(settings, 'SOCIAL_LINKS', {})
-    return render(request, 'pages/social.html', {'links': links})
+    links = getattr(settings, "SOCIAL_LINKS", {})
+    return render(request, "pages/social.html", {"links": links})
