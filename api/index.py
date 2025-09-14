1265931e79b79c178d08ad472812466c38a4ac18
Dionisis Magos <fridimagician@gmail.com>
2025-09-14 23:00:12 +0300

    Fix vercel.json (remove functions runtime) & add python entrypoint

--- api/index.py
+++ api/index.py
@@ -1,7 +1,6 @@
 # api/index.py
 import os
-from asgiref.wsgi import WsgiToAsgi
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-app = WsgiToAsgi(get_wsgi_application())
+app = get_wsgi_application()  # το Vercel ζητά μεταβλητή 'app'
