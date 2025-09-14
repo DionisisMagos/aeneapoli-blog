50726e7f29356767df2330c209cf8ec3e36a18cb
Dionisis Magos <fridimagician@gmail.com>
2025-09-14 17:40:23 +0300

    Vercel: add api/wsgi.py and vercel.json

---
+++ api/wsgi.py
@@ -0,0 +1,6 @@
+# api/wsgi.py
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+app = get_wsgi_application()  # το Vercel runtime ψάχνει μεταβλητή 'app'
